6f371b0df1b11a73a2dde943e56b937f269d2749
Jarrett Cruger <jcrugzz@gmail.com>
2015-05-09 23:33:29 -0700

    [api] use errs for proper serialized errors and allow event dispatching

--- forkee.js
+++ forkee.js
@@ -1,6 +1,9 @@
 
 var EE = require('events').EventEmitter;
+var errs = require('errs');
 var util = require('util');
+
+var extend = util._extend;
 
 module.exports = Forkee;
 
@@ -8,6 +11,7 @@ util.inherits(Forkee, EE);
 
 function Forkee (callback) {
   if (!(this instanceof Forkee)) return new Forkee(callback);
+  EE.call(this);
 
   this._callback = callback && typeof callback == 'function'
     ? callback
@@ -15,6 +19,14 @@ function Forkee (callback) {
 
   process.on('uncaughtException', this._dieWithError.bind(this));
   process.on('message', this._onMessage.bind(this));
+
+  //
+  // Send special event objects for logging purposes if an "event" event is emit
+  // on us
+  //
+  this.on('event', function (type, object) {
+    process.send(extend({ __event: type }, object));
+  });
 
 }
 
@@ -28,16 +40,18 @@ Forkee.prototype._onMessage = function (message) {
     : this._callback(message, this._respond.bind(this));
 };
 
+Forkee.prototype.notify = function (type, object) {
+  this.emit('event', type, object);
+};
+
 Forkee.prototype._respond = function (err, msg) {
   msg = msg || {};
+
   //
-  // Form a ghetto error object since we cannot serialize error objects
+  // Use Errs so that we create a serializable error object
   //
   if (err) {
-    msg.error = {
-      message: err.message,
-      stack: err.stack
-    };
+    msg.error = errs.merge(err, { child: true });
   }
 
   process.send(msg);
@@ -49,8 +63,11 @@ Forkee.prototype._dieWithError = function (err) {
   // We attempt to send an error here but its ok if we don't as `fork`
   // will understand that something bad happened
   //
-  process.send({ error: { message: err.message, stack: err.stack }});
-  process.exit(1);
-};
-
-
+  process.send({ error: errs.merge(err, { uncaughtException: true, child: true }) });
+  //
+  // Try and ensure we end before we force exit on uncaughtExceptions
+  //
+  setTimeout(function () {
+    process.exit(1);
+  }, 100).unref();
+};*/
